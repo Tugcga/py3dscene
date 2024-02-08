f0409f0fb5921fd11861d6d5a9dbf809657ecf39
Shekn <cgproggame@gmail.com>
2024-02-08 11:41:24 +0500

    Filter image name when imported from gltf

--- py3dscene/io/gltf_import/import_image.py
+++ py3dscene/io/gltf_import/import_image.py
@@ -7,17 +7,18 @@ from py3dscene.bin.tiny_gltf import write_png  # type: ignore
 def import_images(gltf_model: GLTFModel,
                   gltf_scene: GLTFScene,
                   file_path: str) -> dict[int, str]:
+    file_path_norm: str = file_path.replace("/", "\\")
     images_map: dict[int, str] = {}
     images_count: int = len(gltf_model.images)
     for i in range(images_count):
-        gltf_image = gltf_model.images[i]
-        image_uri = gltf_image.uri
+        gltf_image: GLTFImage = gltf_model.images[i]
+        image_uri: str = gltf_image.uri
         # calculate image path
         # if uri is empty, then image is embedded
         # in other case the path already exists
-        image_path = ""
-        last_slash = file_path.rfind("/")
-        last_point = file_path.rfind(".")
+        image_path: str = ""
+        last_slash: int = file_path_norm.rfind("\\")
+        last_point: int = file_path_norm.rfind(".")
         if len(image_uri) == 0:
             # extract the image
             image_width: int = gltf_image.width
@@ -26,23 +27,26 @@ def import_images(gltf_model: GLTFModel,
             image_pixel_type: int = gltf_image.pixel_type
             image_bits: int = gltf_image.bits
 
-            image_name: str = gltf_image.name.split("\\")[-1]
+            gltf_image_name: str = gltf_image.name
+            gltf_image_name = gltf_image_name.replace("/", "\\").replace(".", "_")
+            # the name may contains the path to the file, so, get only the last part of the path
+            image_name: str = gltf_image_name.split("\\")[-1]
             if len(image_name) == 0:
                 image_name = "texture_" + str(i)
-            image_folder = file_path[:last_slash] + "/" + file_path[last_slash + 1: last_point] + "_textures/"
+            image_folder: str = file_path_norm[:last_slash] + "\\" + file_path_norm[last_slash + 1: last_point] + "_textures\\"
             # create directory if it does not exists
             os.makedirs(image_folder, exist_ok=True)
             image_path = image_folder + image_name + ".png"
             # skip if the file already exists
             if not os.path.isfile(image_path):
-                is_write = write_png(image_path, image_width, image_height, image_components, gltf_image.image)
+                is_write: bool = write_png(image_path, image_width, image_height, image_components, gltf_image.image)
                 if not is_write:
                     # fail to write the texture
                     image_path = ""
         else:
             # we assume that the path is relative to the input file
             # form new relative path
-            image_path = file_path[:last_slash] + "/" + image_uri
+            image_path = file_path_norm[:last_slash] + "\\" + image_uri
 
         if len(image_path) > 0:
             images_map[i] = image_path
